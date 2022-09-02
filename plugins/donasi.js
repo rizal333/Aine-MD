a3d2c1249602329c74155bd3dd6c62a82f301cc4
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-09-02 15:29:55 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,20 @@
 let handler = async m => m.reply(`
+     *Silahkan kak*
+
 ╭─「 Donasi • Pulsa 」
 │ •  [0896-1872-8196]
 │ •  [0878-9287-0587]
+│ • Dan bisa melalui E-wallet di bawah
 ╰────
+
+╭─「 Donasi • E-wallet 」
+│ • Dana : [0896-1872-8196]
+│ • Gopay : [0878-9287-0587]
+│ • Ovo : [0896-8172-1876]
+│ • Qris : *Hubungi Onwer kami*
+│
+│ • Terima kasih kak♡
+╰────\n\n© NR-Rfky28
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
