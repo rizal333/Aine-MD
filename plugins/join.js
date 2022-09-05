347dbe6ba595fefe35c115247da01c7d0b3fcfcd
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-09-05 20:29:40 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, text, isOwner, groupMetadata }) => {
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
 }
 //handler.help = ['join <chat.whatsapp.com>']
-//handler.tags = ['premium']
+//handler.tags = ['owner']
 
 handler.command = /^join$/i
 handler.owner = true
