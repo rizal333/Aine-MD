cc601207d4c68c5a31913826afbd4fe13ab38b55
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-09-06 15:29:22 +0700

    Update exp.js

--- plugins/exp.js
+++ plugins/exp.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`*Nama:* %name\n*Your Exp:*${global.db.data.users[who].exp}\n*Role:* %role`)
+    m.reply(`*💌 Nama:* ${name}\n*💾 Your Exp:*${global.db.data.users[who].exp}\n*⚒️ Role:* ${Role}`)
 }
 handler.help = ['exp [@user]']
 handler.tags = ['xp']
