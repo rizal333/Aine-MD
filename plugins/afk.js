1513239b8830c0d1892f70464c3491a711d38c16
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-09-06 14:24:34 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -3,7 +3,7 @@ let handler = async (m, { text }) => {
   user.afk = + new Date
   user.afkReason = text
   m.reply(`
-${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
+${conn.getName(m.sender)} 〖 is now AFK 〗\n\n*Nama:* %name\n*Alasan:*${text ? ': ' + text : ''}\\ ⫹⫺ © NR-Rifky28\nVersi : %version
 `)
 }
 handler.help = ['afk [alasan]']
