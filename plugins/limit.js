f852c6d8695119c48d0d61ca984491cfb1b64f05
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-09-06 14:30:02 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`${global.db.data.users[who].limit} Limit Left ಥ_ಥ`)
+    m.reply(`*Name:* %name\n*Limit Tersisa:*${global.db.data.users[who].limit} `)
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
